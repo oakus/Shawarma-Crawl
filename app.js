a10c08b7aa0edb4ec2155b52b90aaac53e08c7e3
Oliver <smithoe@hotmail.com>
2019-08-31 14:55:10 -0400

    Connect to MongoAtlas

--- app.js
+++ app.js
@@ -17,7 +17,15 @@ var commentRoutes       = require("./routes/comments"),
     indexRoutes         = require("./routes/index")
 
        
-mongoose.connect("mongodb://localhost:27017/shawarma_crawl", {useNewUrlParser: true});
+mongoose.connect("mongodb+srv://o3smith:oQupOOpdH21DdCuE@cluster0-ydbys.mongodb.net/shawarma_crawl?retryWrites=true&w=majority", {
+    useNewUrlParser: true,
+    useCreateIndex : true
+}). then(() => {
+    console.log("connected to DB!");
+}).catch(err => {
+    console.log('ERROR:', err.message);
+});
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
